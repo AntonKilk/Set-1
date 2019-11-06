ad2a821545abd1699e90c699420b9a267cfba06a
AntonKilk <51739460+AntonKilk@users.noreply.github.com>
2019-11-06 11:24:15 +0200

    Create task_1.1.js

---
+++ task_1.1.js
@@ -0,0 +1,12 @@
+// Implement an immutable merge (shallow merge).
+
+
+function merge(o1, o2) {
+  ???
+}
+
+let o1 = {x: "x1"}
+let o2 = {y: "y2", x: "x2"}
+
+console.log(merge(o1, o2)) // {y: "y2", x: "x2"}
+console.log(merge(o2, o1)) // {y: "y2", x: "x1"}
